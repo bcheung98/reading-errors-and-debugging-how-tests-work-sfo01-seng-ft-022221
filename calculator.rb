075772103b6f4f65b49c34c22e9eb9327d84e34c
Brian Cheung <briantcheung@gmail.com>
2021-01-12 02:13:16 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,6 @@
 first_number = 42
 second_number = 1027
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
